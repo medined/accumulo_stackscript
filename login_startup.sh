15e22d425f6e406ed0bf3415dde3b728184c39e5
medined <david.medinets@gmail.com>
2012-12-19 01:06:11 -0500

    add an alias that i use all the time.

--- login_startup.sh
+++ login_startup.sh
@@ -5,3 +5,5 @@ export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-i386
 export KAFKA_HOME=/usr/local/kafka
 export ZOOKEEPER_HOME=/usr/local/zookeeper
 export PATH=$ACCUMULO_HOME/bin:$HADOOP_PREFIX/bin:$JAVA_HOME/bin:$KAFKA_HOME/bin:$ZOOKEEPER_HOME/bin:$DEFAULT_PATH
+
+alias la="ls -lah"
