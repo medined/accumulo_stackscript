e60c4b858f856d26919eeb0950789c3972ead2c4
David Medinets <david.medinets@gmail.com>
2013-04-26 00:19:42 -0400

    install meld so I have a visual diff tool

--- install_accumulo_in_home_directory.sh
+++ install_accumulo_in_home_directory.sh
@@ -95,7 +95,7 @@ echo "deb-src http://ppa.launchpad.net/natecarlson/maven3/ubuntu precise main" |
 sudo apt-get update
 
 sudo apt-get -y install curl maven2 openssh-server openssh-client terminator
-sudo apt-get -y install openjdk-6-jdk subversion screen g++ make
+sudo apt-get -y install openjdk-6-jdk subversion screen g++ make meld
 sudo apt-get -y --force-yes install maven3
 
 # remove the symbolic link to maven2. You can still access it via /usr/share/maven2/bin/mvn
